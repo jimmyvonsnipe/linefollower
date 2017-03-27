d367454208e3188d75be59c85494dd4bd935220e
heartworm <shravanlal98@gmail.com>
2017-03-27 14:33:35 +1000

    per-sensor calibration

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 const float POSITIONS[] = {POS_LIN1, POS_LIN2, POS_LIN3, POS_LIN4, POS_LIN5};
 const uint8_t MUXES[] = {MUX_LIN1, MUX_LIN2, MUX_LIN3, MUX_LIN4, MUX_LIN5};
 volatile uint16_t readings[] = {0, 0, 0, 0, 0}; //10 bit output from the ADC
+volatile uint32_t adjusted[] = {0, 0, 0, 0, 0}; //10 bit output from the ADC
+volatile uint16_t maxes[] = {0, 0, 0, 0, 0}; //10 bit output from the ADC
+volatile uint16_t mins[] = {1023, 1023, 1023, 1023, 1023}; //10 bit output from the ADC
 uint8_t curMux = 0;
 uint16_t minLine = 1023;
 uint16_t maxLine = 0;
@@ -21,7 +24,7 @@ int main() {
 		char* str = "XXXX";
 		lcdClearBuffer();
 		for (int i = 0; i < 5; i++) {
-			uint16_t reading = readings[i];
+			uint16_t reading = adjusted[i];
 			itoa(reading, str, 10); //base 10 decimal
 			uint8_t row = i > 2 ? 1 : 0;
 			uint8_t col = i > 2 ? (i - 3) * 5 : i * 5;
@@ -30,8 +33,8 @@ int main() {
 		uint16_t pos = getCoL();
 		itoa(pos, str, 10);
 		lcdPutString(10, 1, str);
-		if (col < 1500)	lcdPutString(14, 1, "<-");
-		else if (col > 2500)	lcdPutString(14, 1, "->");
+		if (pos < 1500)	lcdPutString(14, 1, "<-");
+		else if (pos > 2500)	lcdPutString(14, 1, "->");
 		else	lcdPutString(14, 1, "||");
 
 		lcdDraw();
@@ -56,8 +59,8 @@ void setMux(int mux) {
 ISR(ADC_vect) {
 	uint16_t reading = ADC;
 	readings[curMux] = reading;
-	if (reading < minLine) minLine = reading;
-	if (reading > maxLine) maxLine = reading;
+	if (reading < mins[curMux]) mins[curMux] = reading;
+	if (reading > maxes[curMux]) maxes[curMux] = reading;
 	
 	curMux = (curMux + 1) % ADC_NUMBER;
 	setMux(curMux);
@@ -87,15 +90,21 @@ uint16_t getCoL() {
 	uint32_t mass = 0;
 	uint32_t spreadMin = 1;
 	uint32_t spreadMax = 0;
-	uint16_t range = maxLine - minLine;
 	
 	uint32_t centerValue = (((uint32_t)(ADC_NUMBER-1)) * 1000)/2;
-	if (range == 0) return centerValue;
 	
 	//floats have been replaced with 1000 int precision
 	for (uint8_t i = 0; i < ADC_NUMBER; i++) {
-		uint32_t adjReading = ((readings[i] - minLine) * 1000) / (range);
+		uint32_t range = maxes[i] - mins[i];
+		if (range == 0) return centerValue;
+		
+		
+		uint32_t adjReading = (((uint32_t)readings[i] - mins[i]) * 1000);
+		adjReading = adjReading / (range);
+		//uint32_t adjReading = ((uint32_t)readings[i] * 1000) / 1023;
 		adjReading = adjReading == 0 ? 1 : adjReading;
+
+		adjusted[i] = adjReading;
 		//fltReading = fltReading == 1 ? 0.01 : 1 - fltReading;
 		//uint16_t reading = readings[i] < 1023 ? (1023 - readings[i]) : 1;
 		//float fltReading =  reading / 1023.0;
